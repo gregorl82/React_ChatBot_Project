b46090ab4a16e11970f1c632d73a70e9d468b1f7
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-25 18:23:18 +0000

    Displays exercises when navigating to route

--- client/src/components/reminders/ReminderSelect.js
+++ client/src/components/reminders/ReminderSelect.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import '../../App.css'
 import AppointmentList from './appointments/AppointmentList.js';
+import ExerciseList from './exercise/ExerciseList.js';
 import {Link} from 'react-router-dom';
 import ReminderNavigation from '../navigation/ReminderNavigation';
 
@@ -23,6 +24,10 @@ render(){
             }}>
             </Route>
 
+            <Route exact path="/reminders/exercises" render={() => {
+              return <ExerciseList exercises={this.props.exercises} />
+            }}>
+            </Route>
 
 
 
