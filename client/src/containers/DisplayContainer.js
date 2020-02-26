a31e7a61ba9cf4ba4e82449848c3841a3e771508
Andrew Binnie <binnieandrew@gmail.com>
2020-02-26 09:49:52 +0000

    fixing styling for presentations

--- client/src/containers/DisplayContainer.js
+++ client/src/containers/DisplayContainer.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ContactList from '../components/contacts/ContactList';
 import Puzzle from '../components/puzzles/Puzzle';
 import Navigation from '../components/navigation/Navigation.js';
-import Reminder from '../components/reminders/ReminderList'
 import PhotoSlider from '../components/photos/photo.js';
 import ContactDetail from '../components/contacts/ContactDetail.js';
 import ReminderSelect from '../components/reminders/ReminderSelect';
